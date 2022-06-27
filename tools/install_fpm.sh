dba4aa7f14a1a53a70d8798f28a30de79ad69a1a
Federico E. Benelli <fedebenelli@outlook.com>
2022-06-27 09:13:23 -0300

    fpm scripts

--- tools/install_fpm.sh
+++ tools/install_fpm.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-# Install script for fpm
-wget https://github.com/fortran-lang/fpm/releases/download/v0.6.0/fpm-0.6.0-linux-x86_64 -O fpm
-chmod +x fpm
-mv fpm ~/.local/bin
+install() {
+    wget https://github.com/fortran-lang/fpm/releases/download/v0.6.0/fpm-0.6.0-linux-x86_64 -O fpm
+    chmod +x fpm
+}
+
+run_test() {
+    ./fpm test
+}
+
+
+case $1 in
+    "install")  install;;
+    "test") run_test;;
+esac
