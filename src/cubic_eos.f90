a4c04053b10fce2118d33298e8f5c1d56c3e03ef
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-07-01 10:52:11 -0300

    still not sure if this should be private or public

--- src/cubic_eos.f90
+++ src/cubic_eos.f90
@@ -22,12 +22,12 @@ module cubic_eos
 
    type, extends(pure_compound) :: pr
       !! Peng-Robinson EoS
-      real(8), private :: del1 = 1 !! \[\delta_1\] parameter.
+      real(8) :: del1 = 1 !! \[\delta_1\] parameter.
    end type pr
-   
+
    type, extends(pure_compound) :: srk
       !! Soave-Redlich-Kwong EoS
-      real(8), private :: del1 = 1 + sqrt(2.d0) !! \[\delta_1\] parameter.
+      real(8) :: del1 = 1 + sqrt(2.d0) !! \[\delta_1\] parameter.
    end type srk
 
    type, extends(pure_compound) :: rkpr
