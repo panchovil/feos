987d032aa7e10f972f032fb96431ee10b1b15ef9
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-07-01 11:33:44 -0300

    moved the mixing rule, with it's attributes, inside of the mixing rule object. This gives more extensibility to use with cubic mixing rules.

--- src/mixture.f90
+++ src/mixture.f90
@@ -6,14 +6,7 @@ module multicomponent_eos
 
    type :: mixture
       class(pure_compound), allocatable :: compounds(:)
-      class(kij_constant), allocatable :: kij_calculator
-      class(lij_constant), allocatable :: lij_calculator
-      real(wp), allocatable :: kij_matrix(:, :)
-      real(wp), allocatable :: lij_matrix(:, :)
-      real(wp), allocatable :: aij(:, :)
-      real(wp), allocatable :: bij(:, :)
-         contains
-            procedure :: mix => mix
+      class(quadratic), allocatable :: mixing_rule
    end type mixture
 
 contains
