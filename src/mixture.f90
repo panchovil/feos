b3b769618afbb53b6439afc057d0102430f55de7
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-07-04 16:15:52 -0300

    private by default, added concentrations vector

--- src/mixture.f90
+++ src/mixture.f90
@@ -2,11 +2,15 @@ module multicomponent_eos
    use constants
    use mixing_rules
    use cubic_eos
+
    implicit none
+   private
+
+   public :: mixture
 
    type :: mixture
       class(pure_compound), allocatable :: compounds(:)
+      real(wp), allocatable :: concentrations(:)
       class(quadratic), allocatable :: mixing_rule
    end type mixture
-
 end module multicomponent_eos
