85f8e1087c3c4f06c685b90ba43d0dbac36dd695
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-07-04 10:32:59 -0300

    more complex fpm usage

--- tools/install_fpm.sh
+++ tools/install_fpm.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+FPM=$(which fpm || ./fpm)
+DESIRED_COVERAGE=90
+
+DID_TEST=0
+
+echoerr() { echo -e "$@" 1>&2; }
+
+green() {
+    echo -e "\e[1;32m$@\e[m"
+}
+
+red() {
+    echo -e "\e[1;31m$@\e[m"
+}
 
 install() {
     wget https://github.com/fortran-lang/fpm/releases/download/v0.6.0/fpm-0.6.0-linux-x86_64 -O fpm
@@ -6,10 +20,52 @@ install() {
 }
 
 run_test() {
-    ./fpm test --flag "--coverage"
+    DID_TEST=1
+    echo Checking tests files names...
+    NAMING_ERRORS=0
+    for file in $(find test/*f90); do
+        filename="$(basename $file)"
+        prefix="$(echo $filename | cut -d '_' -f1)"
+
+        if [ "$prefix" = "test" ]; then
+            green "$file [OK]"
+        else
+            NAMING_ERRORS=$((NAMING_ERRORS + 1))
+            red "$file [X]"
+        fi
+    done
+
+    [ $i -ge 1 ] && echoerr "There are wrongly named files in the test directory"
+
+    echo Running tests...
+    $FPM test --flag "--coverage"
 }
+
+run_coverage() {
+    gcovr --exclude "build" --exclude "test"
+    coverage=$(gcovr --exclude "build" --exclude "test" \
+        | grep TOTAL \
+        | awk '{print $4}' \
+        | sed 's/%//')
+    if [ $coverage -le $DESIRED_COVERAGE ]; then
+        echoerr $(red Desired coverage not reached)
+    fi
+}
+
+resumee() {
+    [ DID_TEST = 1 ] &&
+        echo There has been $NAMING_ERRORS test naming errors
+}
+
 
 case $1 in
     "install")  install;;
     "test") run_test;;
+    "coverage") run_coverage;;
+    *)
+        #install
+        run_test
+        run_coverage;;
 esac
+
+resumee
