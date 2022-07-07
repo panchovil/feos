8a6188640fb8344fe2ba658a0460966de1dbe4e8
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-07-07 14:24:27 -0300

    goodbye comment

--- src/cubic_eos.f90
+++ src/cubic_eos.f90
@@ -167,30 +167,4 @@ contains
       end select
    end subroutine a_parameter
 
-   !subroutine d1nder(nc, n, d1, d1_mix, dD1dni, dD12dnij2)
-   !   !! delta_1 of the mixture and it's compositional derivatives.
-   !   integer, intent(in) :: nc !! Number of components.
-   !   real(wp), intent(in) :: n(nc) !! Array of mole numbers for each component.
-   !   real(wp), intent(in) :: d1(nc) !! Array of delta 1 parameters for each component.
-   !   real(wp), intent(out) :: d1_mix !! Mixture's delta 1
-   !   real(wp), intent(out) :: dD1dni(nc) !! delta1 parameter first derivative with composition.
-   !   real(wp), intent(out) :: dD12dnij2(nc, nc) !! delta1 parameter second derivative with composition.
-
-   !   integer :: i, j
-   !   real(wp) :: totn ! Total number of moles
-
-   !   d1_mix = 0.0_wp
-   !   d1_mix = sum(n*d1)
-
-   !   totn = sum(n)
-   !   D1_mix = D1_mix/totn
-   !   dD1dni = (d1 - d1_mix)/totn
-
-   !   do i = 1, nc
-   !      do j = 1, nc
-   !         dD12dnij2(i, j) = (2.0_wp*d1_mix - d1(i) - d1(j))/totn**2
-   !      end do
-   !   end do
-   !end subroutine d1nder
-
 end module cubic_eos
