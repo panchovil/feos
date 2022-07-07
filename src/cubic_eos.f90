5d541e5667a84c724e08a7f4b474d6592eb16089
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-07-07 14:24:06 -0300

    critical constants and parameters routines

--- src/cubic_eos.f90
+++ src/cubic_eos.f90
@@ -26,6 +26,8 @@ module cubic_eos
       real(wp) :: da2dt2 = 0 !! Atractive parameter second derivative with tempetarue
    contains
       procedure :: a_t => a_parameter
+      procedure :: get_params => get_params
+      procedure :: get_critical_constants => get_critical_constants
    end type pure_compound
 
    type, extends(pure_compound) :: pr
@@ -43,6 +45,99 @@ module cubic_eos
    end type rkpr
 
 contains
+
+   subroutine get_params(self)
+      class(pure_compound) :: self
+
+      real(wp) :: Zc, OMa, OMb, RT, Vceos
+
+      RT = R*self%Tc
+
+      associate( k => self%k, w => self%w )
+      select type (self)
+      class is (pr)
+         call get_Zc_OMa_OMb(self%del1, Zc, OMa, OMb)
+         self%ac = OMa*RT**2/self%Pc
+         self%b = OMb*RT/self%Pc
+         Vceos = Zc*RT/self%Pc
+         ! m constant to calculate a depending on temperature
+         if (w <= 0.491) then
+            ! m from PR
+            k = 0.37464 + 1.54226*w - 0.26992*w**2
+         end if
+         if (w > 0.491) then
+            ! PR78
+            k = 0.379642 + 1.48503*w - 0.164423*w**2 + 0.016666*w**3
+         end if
+
+      class is (srk)
+         call get_Zc_OMa_OMb(self%del1, Zc, OMa, OMb)
+         self%ac = OMa*RT**2/self%Pc
+         self%b = OMb*RT/self%Pc
+         Vceos = Zc*RT/self%Pc
+         k = 0.48 + 1.574*w - 0.175*w**2
+
+      class is (rkpr)
+         call get_Zc_OMa_OMb(self%del1, Zc, OMa, OMb)
+         self%ac = OMa*RT**2/self%Pc
+         self%b = OMb*RT/self%Pc
+         Vceos = Zc*RT/self%Pc
+         print *, "Not implemented yet"
+      end select
+      end associate
+   end subroutine get_params
+
+
+   subroutine get_critical_constants(self)
+      class(pure_compound) :: self
+      
+      real(wp) :: OMa, OMb, Zc, al, be, ga, Vceos
+
+      select type (self)
+      class is (pr)
+         call get_Zc_OMa_OMb(self%del1, Zc, OMa, OMb)
+
+         self%Tc = OMb*self%ac/(OMa*R*self%b)
+         self%Pc = OMb*R*self%Tc/self%b
+         Vceos = Zc*R*self%Tc/self%Pc
+
+         al = -0.26992
+         be = 1.54226
+         ga = 0.37464 - self%k
+
+         self%w = 0.5*(-be + sqrt(be**2 - 4*al*ga))/al
+      class is (srk)
+         call get_Zc_OMa_OMb(self%del1, Zc, OMa, OMb)
+
+         self%Tc = OMb*self%ac/(OMa*R*self%b)
+         self%Pc = OMb*R*self%Tc/self%b
+         Vceos = Zc*R*self%Tc/self%Pc
+
+         al = -0.175
+         be = 1.574
+         ga = 0.48 - self%k
+         self%w = 0.5*(-be + sqrt(be**2 - 4*al*ga))/al
+      class is (rkpr)
+         print *, "Not implemented yet"
+      end select
+   end subroutine get_critical_constants
+
+   
+   subroutine get_Zc_OMa_OMb(del1, Zc, OMa, OMb)
+      real(wp), intent(in) :: del1 !! RKPR delta_1 parameter
+      real(wp), intent(out) :: Zc !! Critical compressibility factor
+      real(wp), intent(out) :: OMa !! 
+      real(wp), intent(out) :: OMb !!
+
+      real(wp) :: d1, y
+
+      d1 = (1.d0 + del1**2.d0)/(1.d0 + del1)
+      y = 1.d0 + (2.d0*(1.d0 + del1))**(1.0d0/3.d0) + (4.d0/(1.d0 + del1))**(1.0d0/3)
+      OMa = (3.d0*y*y + 3.d0*y*d1 + d1**2.d0 + d1 - 1.0d0)/(3.d0*y + d1 - 1.0d0)**2.d0
+      OMb = 1.d0/(3.d0*y + d1 - 1.0d0)
+      Zc = y/(3.d0*y + d1 - 1.0d0)
+   end subroutine get_Zc_OMa_OMb
+
 
    subroutine a_parameter(self, T)
       !! Calculate the atractive parameter at T temperature.
