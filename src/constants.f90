61f5f376f0133e21410bdde2a2892d84839e1b65
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-07-07 14:14:45 -0300

    format

--- src/constants.f90
+++ src/constants.f90
@@ -1,7 +1,10 @@
 module constants
+   !! All constants used in the library
+
    implicit none
-   integer, parameter :: wp = 8
-   real(wp), parameter :: RGAS = 0.08314472d0
+
+   integer, parameter :: wp = 8 !! Working precision
+   real(wp), parameter :: R = 0.08314472d0 !! Gas constant J/molK
    real(wp), parameter :: A0 = 0.0017, B0 = 1.9681, C0 = -2.7238
    real(wp), parameter :: A1 = -2.4407, B1 = 7.4513, C1 = 12.504
    real(wp), dimension(6) :: Dfit = [0.428363, 18.496215, 0.338426, &
