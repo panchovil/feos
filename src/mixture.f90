f8bffdcb6d214b2072be71fe4af1edea3f0c8896
Federico E. Benelli <fedebenelli@outlook.com>
2022-09-23 09:50:26 -0300

    this is only to be able to compile, mixture object still needs design

--- src/mixture.f90
+++ src/mixture.f90
@@ -1,18 +1,22 @@
 module multicomponent_eos
-
-   use constants
-   use mixing_rules
-   use cubic_eos
-
+   use constants, only: wp
+   use mixing_rules, only: ConstantBIP
+   use cubic_eos, only: CubicEoS
+   
    implicit none
 
    private
 
    public :: mixture
 
-   type :: mixture
-      class(pure_compound), allocatable :: compounds(:)
-      real(wp), allocatable :: concentrations(:)
-      class(quadratic), allocatable :: mixing_rule
+   type :: mixture(n)
+      integer, len :: n
+      real(wp) :: P
+      real(wp) :: V
+      real(wp) :: T
+      type(CubicEoS) :: as_a_fluid !! As a fluid representation of the mixture
+      type(CubicEoS) :: compounds(n)
+      !real(wp), allocatable :: concentrations(:)
+      type(ConstantBIP) :: mixing_rule(n)
    end type mixture
 end module multicomponent_eos
