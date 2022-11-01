730d7d8741332d059a405b502757124a394cc1ec
Federico E. Benelli <fedebenelli@outlook.com>
2022-11-01 10:15:08 -0300

    residual helmholtz, pressure, lnfug and volume solver procedures added this needs some profiling, run's a bit slow

--- src/fluid.f90
+++ src/fluid.f90
@@ -34,11 +34,17 @@ module fluid
         procedure :: set_to => set_to 
         !> Copy the fluid
         procedure :: copy => copy 
+        !> Pressure calculation
+        procedure :: pressure => pressure
+        !> lnphi calculation
+        procedure :: lnphi => fugacity
+        !> Volume solver
+        procedure :: vsolve => solve_volume
     end type CubicFluid
 
 contains
 
-    pure subroutine set_to(self, pressure, volume,  temperature)
+    pure subroutine set_to(self, pressure, volume, temperature)
         !! Change a fluid PVT state.
         !!
         !! Works based on specification. Iterative procedures will be
@@ -50,10 +56,12 @@ contains
         real(wp), optional, intent(in) :: temperature !! Temperature [K]
 
         if (present(volume)) then
+            self%v = volume
             self%components%v = volume
         end if
 
         if (present(temperature)) then
+            self%t = temperature
             self%components%t = temperature
         end if
     end subroutine
@@ -68,6 +76,9 @@ contains
         allocate(CubicFluid :: new)
         new%components = self%components
         new%mixing_rule = self%mixing_rule
+        new%p = self%p
+        new%v = self%v
+        new%t = self%t
     end function
     
     elemental function residual_helm(self, v, t) result(ar)
@@ -126,4 +137,142 @@ contains
         ar = rkpr_residual_helmholtz(n, moles, v, t, d, d1, b)
     end function residual_helm
 
+    elemental function pressure(self, v, t) result(p)
+        !! Pressure calculation
+        class(CubicFluid), intent(in) :: self
+        !> Volume [dm^3]
+        real(wp), intent(in) :: v 
+        !> Temperature [K]
+        real(wp), intent(in) :: t 
+        !> Pressure and it's derivatives
+        type(scalar_property) :: p 
+        
+        type(scalar_property) :: ar 
+
+        integer :: n
+
+        real(wp) :: moles
+        
+        n = size(self%components)
+        moles = sum(self%components%moles)
+
+        p = null_scalar_property(n)
+
+        ar = self%residual_helmholtz(v, t)
+
+        p%val = -ar%dv + moles * R * t / v
+        p%dt = -ar%dtv + moles * R / v
+        p%dv = -ar%dv2 - moles * R * t / v**2
+        p%dn = -ar%dvn + R * t / v
+    end function pressure
+    
+    pure function fugacity(self, v, t) result(lnphi)
+        !! Pressure calculation
+        class(CubicFluid), intent(in) :: self
+        !> Volume [dm^3]
+        real(wp), intent(in) :: v 
+        !> Temperature [K]
+        real(wp), intent(in) :: t 
+        !> Pressure and it's derivatives
+        type(scalar_property), allocatable :: lnphi(:)
+        
+        type(scalar_property) :: ar, p
+
+        integer :: n
+
+        real(wp) :: moles, RT
+
+        n = size(self%components)
+        moles = sum(self%components%moles)
+
+        RT = R * t
+        ar = null_scalar_property(n)
+        p = null_scalar_property(n)
+        allocate(lnphi(n))
+        lnphi = null_scalar_property(n)
+
+        ar = self%residual_helmholtz(v, t)
+        p = self%pressure(v, t)
+
+        lnphi%val = ar%dn/RT - log((p*v)/(RT))
+        lnphi%dt = ar%dtn/RT
+    end function fugacity
+
+    recursive pure function solve_volume(self, p_obj, t, root, max_it) result(v)
+        !! Solve volume root for a specified pressure at the fluid's temperature
+
+        !> Fluid
+        class(CubicFluid), intent(in) :: self
+        !> Objective pressure
+        real(wp), intent(in) :: p_obj
+        !> Temperature
+        real(wp), intent(in) :: t
+        !> Desired root, could be either "vapor" or "liquid"
+        character(len=*), optional, intent(in) :: root 
+        !> Max number of iterations, defaults to 50
+        integer, optional, intent(in) :: max_it
+        !> Output volume
+        real(wp) :: v
+        !> fluid at new volume to use inside iterations
+        class(CubicFluid), allocatable :: fluid
+
+        ! Mixture parameters and calculated pressure
+        type(scalar_property) :: d, b, d1, p
+        
+        ! Inside variables
+        real(wp) :: error, v0, z_min, z_max, z
+        integer :: it, maxit
+
+        if (.not. present(max_it)) then
+             maxit = 50
+        else
+            maxit = max_it
+        end if
+        
+        fluid = self%copy()
+
+        call fluid%mixing_rule%mix(fluid%components, d, b, d1)
+
+        z_min = 0
+        z_max = 1 - 0.01 * t / (10000*b)
+
+        if (present(root)) then
+            select case (root)
+            case ("vapor")
+                z = min(0.5_wp, b * p_obj / (r * t))
+                v0 = b/z
+            case ("liquid")
+                z = 0.5_wp
+                v0 = b/z
+            end select
+            p = fluid%pressure(v0, t)
+            error = abs(p - p_obj)
+
+            it = 0
+            do while(error >= errmax .and. it < maxit)
+                it = it + 1
+                v = v0 - (p - p_obj)/p%dv
+                p = fluid%pressure(v, t)
+                v0 = v
+                error = abs(p - p_obj)
+            end do
+        else
+            find_stable: block
+                real(wp) :: vapor_v, liquid_v
+                type(scalar_property) :: vapor_ar, liquid_ar
+
+                vapor_v = fluid%vsolve(p_obj, t, "vapor")
+                liquid_v = fluid%vsolve(p_obj, t, "liquid")
+
+                vapor_ar = fluid%residual_helmholtz(vapor_v, t)
+                liquid_ar = fluid%residual_helmholtz(liquid_v, t)
+
+                if (vapor_ar%dt < liquid_ar%dt) then
+                    v = vapor_v
+                else
+                    v = liquid_v
+                end if
+            end block find_stable
+        end if
+    end function solve_volume
 end module fluid
