a3bd3a7bbef81114a473a3a94913b395522b51c9
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-11-02 15:06:42 -0300

    legacy_main

--- legacy/gen.f90
+++ legacy/gen.f90
@@ -11,61 +11,29 @@ program main
 contains 
    subroutine run(n)
       integer :: i, j, n
-      real(8) :: t=250.d0, aij(n,n), aijdt(n,n), daijdt2(n,n), daijdt(n,n), z(n)
-      real(8) :: d, ddi(n), ddit(n), ddij(n,n), dddt, dddt2
-      real(8) :: Ar, Arv, artv, arv2, arn(n), arvn(n), artn(n), arn2(n, n)
-
-      real(8) :: temps(3) = [200, 250, 300]
+      real(8) :: p, v, t, z(n), ps(500)
       
-      D = 0.d0
-      ddi = 0
-      ddit = 0
-      ddij = 0
-      dddt = 0
-      dddt2 = 0
-
       read (1, *) (z(j), j=1, n)
       read (1, *) nmodel
       call read2PcubicNC(n, nin, nout)
 
-      call aijTder(1, n, T, aij, daijdt, daijdt2)
-      print *, "--------------"
-      print *, "aij"
-      do i=1,n
-         print *, aij(i, :)
+      open(unit=3, file="vs")
+      do i=1,500
+         read(3, *) ps(i)
       end do
-      print *, "--------------"
-      print *, "aij dt"
-      do i=1,n
-         print *, daijdt(i, :)
-      end do
-      print *, "--------------"
-      print *, "aij dt2"
-      do i=1,n
-         print *, daijdt2(i, :)
-      end do
-      print *, "--------------"
-
-
-      call DandTnder(1, n, T, z, D, dDi, dDiT, dDij, dDdT, dDdT2)
-
-      print *, "D"
-      print *, D, ddi(:), shape(ddi)
-      print *, "--------------"
 
       z = z/sum(z) ! normalize
 
-      do i = 1, 3
-         T = 250 ! temps(i)
-         call HelmSRKPR(n, 2, 1, z, 10.0, T, Ar, ArV, ArtV, Arv2, Arn, arVn, ArTn, Arn2)
-         print *, Ar, arv, arv2, artv
-         print *, arn
+      t = 250.d0
+      v = 0.d0
 
-         do j=1,3
-            print *, arn2(:, j)
-         end do
-         
-         stop
+      do i = 1, 500
+         p = ps(i)
+
+         call VCALC(0, n, 2, z, T, P, V)
+
+         print *, v, p
       end do
+
    end subroutine run
 end program main
