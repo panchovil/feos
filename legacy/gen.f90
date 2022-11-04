d23127a9e4fbd3cb7097e00899d136a22d2d7971
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2022-11-04 13:38:52 -0300

    legacy runner does volume roots

--- legacy/gen.f90
+++ legacy/gen.f90
@@ -11,29 +11,40 @@ program main
 contains 
    subroutine run(n)
       integer :: i, j, n
-      real(8) :: p, v, t, z(n), ps(500)
+      real(8) :: p, v, t, z(n)
+      real(8) :: Ar, ArV, ArTV, ArV2, Arn(n), ArVn(n), ArTn(n), Arn2(n, n)
+      real(8), parameter :: R=0.08314472
       
       read (1, *) (z(j), j=1, n)
       read (1, *) nmodel
       call read2PcubicNC(n, nin, nout)
 
-      open(unit=3, file="vs")
-      do i=1,500
-         read(3, *) ps(i)
-      end do
-
       z = z/sum(z) ! normalize
 
-      t = 250.d0
-      v = 0.d0
+      t = 150.d0
+      v = 5.d0
+      
+!      v = 25.05227907637569
+!      call ArVnder(n, 2, 1, z, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
+!      print *, ar, arv
+!
+!      v = 0.050351279383939546
+!      call ArVnder(n, 2, 1, z, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
+!      print *, ar, arv
+!
+!      call exit
 
-      do i = 1, 500
-         p = ps(i)
-
+      do i = 150, 800
+         p = real(i, 8)/1000
          call VCALC(0, n, 2, z, T, P, V)
-
-         print *, v, p
+         call ArVnder(n, 2, 1, z, V, T, Ar, ArV, ArTV, ArV2, Arn, ArVn, ArTn, Arn2)
+         if (i .eq. 488 .or. i .eq. 489) then
+            print  *, (Ar + V*P)/(T*R) - LOG(V)
+         end if
+         print *, v, p, ar 
       end do
 
+      p = sum(z) * R * t/v - arv
+      call VCALC(0, n, 2, z, T, P, V)
    end subroutine run
 end program main
